e97001e1ab2f1b2cd5cf572ac141c337a65aa6ad
Nikodem <upior126@gmail.com>
2023-06-01 18:23:45 +0200

    Dynamic views added

--- mypage/challenges/urls.py
+++ mypage/challenges/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
-    path("february", views.february)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+#dictionary with monthly challenges
+monthly_challenges = {
+    "january": "Eat no sweets for the entire month",
+    "february": "Work out for at least 20 minutes every day!",
+    "march": "Read book for 30 minutes every day!",
+    "april": "Walk for 20 minutes every day",
+    "may": "Cook for yourself four times a week",
+    "june": "Eat no meat for the entire month",
+    "july": "Dont drink any alcohol",
+    "august": "Paint one piece every week",
+    "september": "Dont use your phone more than hour per day",
+    "october": "Work out at least 30 minutes every day!",
+    "november": "Don't watch netflix entire month",
+    "december": "Learn spanish everyday"
+}
+
 
 # Create your views here.
+#function with int month views
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
-
-def index(request):
-    return HttpResponse("This works")
-
-def february(request):
-    return HttpResponse("February")
+#function with monthly_challenge (challenges/<month>) view
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
