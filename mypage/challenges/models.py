6974800f5b5ff063de9a7c0ead48d5c10d943ea7
Nikodem <upior126@gmail.com>
2023-07-04 12:59:29 +0200

    Challenge model added, header styles added

--- mypage/challenges/models.py
+++ mypage/challenges/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    month = models.CharField(max_length=9)
+    rating = models.IntegerField()
