fdde6fb147d38842e18540294c08643fe3ce7467
Nikodem <upior126@gmail.com>
2023-07-05 11:05:16 +0200

    Little changes in Challenge model and migrations added

--- mypage/challenges/models.py
+++ mypage/challenges/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -6,4 +7,7 @@ class Challenge(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     month = models.CharField(max_length=9)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.title} - {self.description} ({self.month}'s challenge with {self.rating} rating)"
